020568d3438f5908aca4b7c599a7a6f3138ead25
Taiki KONNO <s1290115@u-aizu.ac.jp>
2022-07-11 12:12:17 +0900

    HoT:initial revision

---
+++ ex4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int i,countt=0,countf=0;
+
+  srand((int)time(NULL));
+  printf("Tossing a coin...\n");
+  for(i=1;i<=3;i++){
+    if(rand() %2 ==0){
+    printf("Round %d: Tails\n",i);
+    countf++;
+    }else { printf("Round %d: Heads\n",i);
+      countt++;
+    }
+  }
+    printf("Heads: %d, Tails: %d\n",countt,countf);
+
+    return 0;
+  }
+  
+
